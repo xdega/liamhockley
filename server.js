a474bdaad6ccb206c7106e80b4d62a88958beff1
Liam Hockley <xdega121@gmail.com>
2018-12-22 10:15:53 -0600

    Define view diectory in server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 const config = require("config");
+const path = require("path");
 
 app.use(express.static("public"));
 app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index", {
-    message: "Welcome to my personal website",
-    title: "liamhockley.me"
-  });
+  res.render("index", {});
 });
 
 app.listen(config.get("port"), () => {
