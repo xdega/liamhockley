11cff349a68dd5e3f270f5edf134006df8729887
Liam Hockley <xdega121@gmail.com>
2019-02-08 14:30:21 -0600

    Set Up layout for content/article area

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,3 @@
-console.log("Javascript Assets Loaded!");
+console.log("I see you're the curious type...");
+console.log("Greetings fellow \"hacker\"!");
+console.log("I may add something interesting here in the future.");
