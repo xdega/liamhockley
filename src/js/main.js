b9df53f7994ed3c61cce5ce0430272b247a854ad
Liam Hockley <xdega121@gmail.com>
2019-02-08 14:38:19 -0600

    Run eslint --fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
 console.log("I see you're the curious type...");
-console.log("Greetings fellow \"hacker\"!");
+console.log('Greetings fellow "hacker"!');
 console.log("I may add something interesting here in the future.");
